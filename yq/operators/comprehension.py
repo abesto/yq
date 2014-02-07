3e4f913d1e0cd68944a4e0179839cf5b476a7fe0
Zoltán Nagy <abesto@abesto.net>
2014-02-07 07:39:29 +0100

    Test comprehension

--- yq/operators/comprehension.py
+++ yq/operators/comprehension.py
@@ -1,9 +1,17 @@
+from yq.operators.match_error import MatchError
+
+
 class Comprehension(object):
     def __init__(self, op):
         self.op = op
 
     def apply(self, data):
+        if not isinstance(data, list):
+            raise MatchError(self, data, 'tried to apply comprehension %s to non-array' % self)
         retval = []
         for item in data:
             retval.append(self.op.apply(item))
         return retval
+
+    def __repr__(self):
+        return '[%s]' % self.op
