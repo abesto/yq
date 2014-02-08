81c08317d3f4af06a0b10ca936da29530284c98b
Zoltán Nagy <abesto@abesto.net>
2014-02-08 08:41:50 +0100

    Support building empty object, cleaner parsing of .foo[]

--- yq/parser.py
+++ yq/parser.py
@@ -22,32 +22,38 @@ def wtf(ts):
 key = lambda: Word(alphanums + '_')
 
 # Literal values
-literals = oneOf('true false null') | ('"'+Word(alphanums)+'"') | (Word(nums + '- .'))
+literals = oneOf('true false null') | ('"'+Word(alphanums)+'"') | (Word(nums + '- .')).setName('literal')
 literals.setParseAction(lambda ts: Constant(yaml.load(ts[0])))
 
 # Operations
 operation = Forward()
 
 emptyDot = Literal('.').setParseAction(lambda ts: Dot())
-dot = (('.' + key()) |
-       ('.["' + key() + '"]') |
-       ('."' + key() + '"')).setParseAction(lambda ts: Dot(ts[1]))
+dot = (('.' + key()).setName('dot_str') |
+       ('.["' + key() + '"]').setName('dot_subscript_str') |
+       ('."' + key() + '"').setName('dot_quote_str')).setParseAction(lambda ts: Dot(ts[1]))
 
-extract = Literal('[]').setParseAction(Extract)
+extract = (Literal('.[]').setParseAction(Extract).setName('extract') |
+           (dot + '[]').setParseAction(lambda ts: Sequence([ts[0], Extract()])))
 
-subscript = ('[' + delimitedList(Word(nums)) + ']').setParseAction(lambda ts: Subscript(map(int, ts[1:-1])))
-subsequence = ('[' + Optional(Word(nums + '-'), None) + ':' + Optional(Word(nums + '-'), None) + ']').setParseAction(lambda ts: Subsequence(ts[1], ts[3]))
+subscript = ('[' + delimitedList(Word(nums)) + ']')\
+    .setParseAction(lambda ts: Subscript(map(int, ts[1:-1])))\
+    .setName('subscript')
+
+subsequence = ('[' + Optional(Word(nums + '-'), None) + ':' + Optional(Word(nums + '-'), None) + ']')\
+    .setParseAction(lambda ts: Subsequence(ts[1], ts[3]))\
+    .setName('subsequence')
 
 chainable = extract | dot | emptyDot | subscript | subsequence | literals
-chain = OneOrMore(chainable).setParseAction(lambda ts: Sequence(ts.asList()))
+chain = OneOrMore(chainable).setParseAction(lambda ts: Sequence(ts.asList())).setName('chain')
 
 comma = delimitedList(chain).setParseAction(Comma)
 
 projectionItem = (
     (key() + ':' + chain).setParseAction(lambda ts: ProjectionItem(ts[0], ts[2])) |
     key().setParseAction(lambda ts: ProjectionItem(ts[0], Dot(ts[0])))
-)
-projection = ('{' + delimitedList(projectionItem, ',') + '}').setParseAction(lambda ts: Projection(ts[1:-1]))
+).setName('projection_item')
+projection = ('{' + Optional(delimitedList(projectionItem, ',')) + '}').setParseAction(lambda ts: Projection(ts[1:-1])).setName('projection')
 
 comprehension = ('[' + operation + ']').setParseAction(lambda ts: Comprehension(ts[1]))
 
